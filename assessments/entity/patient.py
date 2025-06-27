c527f62c13bf17e5c6bec0e7c2ad32e1053ab5c1
HARITHA H <harithah.2020@gmail.com>
2025-06-27 17:32:07 +0530

    Delete assessments/entity directory

--- assessments/entity/patient.py
+++
@@ -1,8 +0,0 @@
-class Patient:
-    def __init__(self,patient_id,first_name,last_name,dob,gender,contact_number):
-        self.patient_id = patient_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.dob = dob
-        self.gender = gender
-        self.contact_number = contact_number
